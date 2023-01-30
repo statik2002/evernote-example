1c1d575b47ffd7c41c73b3e9ae1cf9d0b7639733
statik2002 <statik2002@mail.ru>
2023-01-30 14:55:29 +0800

    Initial commit

--- config.py
+++ config.py
@@ -1,16 +1,15 @@
-from pydantic import BaseSettings
+import os
+from environs import Env
 
 
-class Settings(BaseSettings):
-    EVERNOTE_CONSUMER_KEY: str
-    EVERNOTE_CONSUMER_SECRET: str
-    EVERNOTE_PERSONAL_TOKEN: str
+class Settings:
 
-    JOURNAL_TEMPLATE_NOTE_GUID: str
-    JOURNAL_NOTEBOOK_GUID: str
-
-    INBOX_NOTEBOOK_GUID: str
-    
-    class Config:
-        env_file = '.env'
-        env_file_encoding = 'utf-8'
+    def __init__(self):
+        env = Env()
+        env.read_env()
+        self.EVERNOTE_CONSUMER_KEY = env('EVERNOTE_CONSUMER_KEY')
+        self.EVERNOTE_CONSUMER_SECRET = env('EVERNOTE_CONSUMER_SECRET')
+        self.EVERNOTE_PERSONAL_TOKEN = env('EVERNOTE_PERSONAL_TOKEN')
+        self.JOURNAL_TEMPLATE_NOTE_GUID = env('JOURNAL_TEMPLATE_NOTE_GUID')
+        self.JOURNAL_NOTEBOOK_GUID = env('JOURNAL_NOTEBOOK_GUID')
+        self.INBOX_NOTEBOOK_GUID = env('INBOX_NOTEBOOK_GUID')
